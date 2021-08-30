0f33f32ec29ab4ca7751dcaa7c044b75164ab714
kBeliczynski <kamcio1975@gmail.com>
2021-08-30 23:46:19 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,19 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent, children: [
+      {path: '', component: RecipeStartComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent},
+      {path: ':id/edit', component: RecipeEditComponent},
+    ]}
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +29,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RecipeDetailComponent,
     RecipeEditComponent,
     RecipeListComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeStartComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
